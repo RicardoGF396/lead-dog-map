ea9e003fa072667d97eaad7b0ef965f406643d0c
Carlos Ernesto González Flores <cgf72793@udelasalle.edu.mx>
2023-05-09 18:08:00 -0600

    Se agregó funcionalidad de listar ubicaciones, pigy

--- javascript/map.js
+++ javascript/map.js
@@ -6,7 +6,28 @@ const latitud = urlParams.get("latitud");
 const longitud = urlParams.get("longitud");
 
 //Consumir de la API todas las sucursales y renderizarlas en el DOM
+let branches =[{}]
+async function getBranches() {
+  const response = await fetch("http://172.18.70.100:4005/api/branches/all");
+  const jsonData = await response.json();
+  branches = jsonData.features;
+  
+}
+await getBranches()
 
+//console.log(branches);
+
+  const cards = branches.map(branch =>{
+    return `
+    <div class="card">
+    <h4 id="sucursal"> ${branch.properties.name}</h4>
+    <p id="gerente" class="manager-label"><b>Manager: </b>${branch.properties.manager_name}</p>
+    <p id="latitud">Latitude: <span>${branch.geometry.coordinates[0]}</span></p>
+    <p id="longitud">Longitud: <span></span>${branch.geometry.coordinates[1]}</p>
+  </div>
+`
+  }).join(" ") 
+document.getElementById('id-component').innerHTML = cards
 //Hacer lógica para encontrar la sucursal más cercana y excluirla del arregjlo que se va a mostrar en
 //la lista de sucursales
 
